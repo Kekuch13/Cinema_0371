9d0a82228396fa5235926657535ff9f920f75a4c
Matvey Emelyanov <kassius2008@gmail.com>
2022-12-25 14:15:07 +0300

    Продажа билетов

---
+++ Forms/src/main/java/forms/BookingForm.java
@@ -0,0 +1,24 @@
+package forms;
+
+import java.util.ArrayList;
+
+public class BookingForm {
+    final String form = "booking";
+    ArrayList<Integer> tickets_ids;
+
+    public BookingForm(ArrayList<Integer> tickets_ids) {
+        this.tickets_ids = tickets_ids;
+    }
+
+    public String getForm() {
+        return form;
+    }
+
+    public ArrayList<Integer> getTickets_ids() {
+        return tickets_ids;
+    }
+
+    public void setTickets_ids(ArrayList<Integer> tickets_ids) {
+        this.tickets_ids = tickets_ids;
+    }
+}
